8d2e34a4547487aeed3f6820012e7690f5845d53
Rahidul Islam <rahiseli@outlook.com>
2024-10-07 16:43:03 +0600

    reverse string updated

--- week3.1/reverse_sting.py
+++ week3.1/reverse_sting.py
@@ -4,7 +4,34 @@
 #     return s[::-1]
 # print(reverse_string("hello"))
 
-def reverse_list(s):
-    return s.reverse()
+# def reverse_list(s):
+#     return s.reverse()
+# print(reverse_list(["h","e","l","l","o"]))
+# print(reverse_list([1,2,3,4,5]))
+# ============================================
+
+# def reverse_list(s):
+#     left =0
+#     right = len(s)-1
+#     while left < right:
+#         s[left], s[right] = s[right], s[left]
+#         left += 1
+#         right -= 1
+#     return s
+# print(reverse_list(["h","e","l","l","o"]))
+# print(reverse_list([1,2,3,4,5]))
+
+# =============================================
+def reverse_list(s:list[str])-> list[str]:
+    left =0
+    right = len(s)-1
+    while left < right:
+        temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        # s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
 print(reverse_list(["h","e","l","l","o"]))
 print(reverse_list([1,2,3,4,5]))
--- week3.1/reverse_string.js
+++ week3.1/reverse_string.js
@@ -1,7 +1,19 @@
 // leetcod -344
 function reverseString(str){
-    const strArr = str.split('')
-    const reversedArr = strArr.reverse()
-    return reversedArr.join('')
+    // const strArr = str.split('')
+    // const reversedArr = strArr.reverse()
+    // return reversedArr.join('')
+    let left = 0
+    let right = str.length-1
+    while(left<right){
+        let temp = str[left]
+        str[left] = str[right]
+        str[right] = temp
+        left ++
+        right --
+    }
+    return str
 }
 console.log(reverseString("hello"))
+console.log(reverseString(["h","e","l","l","o"]))
+console.log(reverseString(["H","a","n","n","a","h"]))
