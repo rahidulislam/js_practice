0ee74a0befbfa6e0316350e249a5a522822576fb
Rahidul Islam <rahiseli@outlook.com>
2025-01-09 11:28:59 +0600

    i learn about constructior function

---
+++ constructor_function.js
@@ -0,0 +1,11 @@
+function User(name){
+    // this = {};
+
+    // add properties to this
+    this.name = name;
+    this.isAdmin=false
+
+    // return this
+}
+let user = new User("John")
+console.log(user)
